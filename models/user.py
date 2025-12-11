fc5483cb499959ec8c55fbeed5d7c621d59fa1ba
Rewaly <haba0818@icloud.com>
2025-12-11 15:57:19 +0900

    月収登録
    user.py
    index.html
    registration.html
    3つの作成

--- models/user.py
+++ models/user.py
@@ -2,8 +2,11 @@ from peewee import Model, CharField, IntegerField
 from .db import db
 
 class User(Model):
-    name = CharField()
-    age = IntegerField()
+    name = CharField() 
+    created_at = IntegerField()
+    workplace = CharField()
+    income = IntegerField()
+
 
     class Meta:
         database = db
